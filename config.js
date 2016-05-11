ac01eae5907b15ec0cc5787809f93fcf3c418696
zane.hitchcox@one.verizon.com <zane.hitchcox@one.verizon.com>
2016-05-11 15:02:34 -0500

    added google oauth

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
     id:     '2ff63c36894be681d76f',
     secret: '2a5745f0a201702ac3a050d56737a4813e2a80d1',
   },
-  facebook: {
-    id:     '',
-    secret: '',
+  google: {
+    id:     '947424859910-suq9a72mpgjf9452joqpe8rl53pte08n.apps.googleusercontent.com',
+    secret: '3uOQ0cvA1sNFaH4gS22GFlzX',
   }
 }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/github/callback', (req, res) => {
+app.get('/github/callback', (req, res) => 
   fetch('https://github.com/login/oauth/access_token',
     {
       method: 'POST',
@@ -33,7 +33,36 @@ app.get('/github/callback', (req, res) => {
   .then(json => (console.log(json), 
     res.redirect('/welcome/'+(json.name || json.username))))
   )
-})
+  .catch(e => (console.log(e), res.status(403).end('Not Authorized')))
+)
+app.get('/google/callback', (req, res) => {
+  fetch('https://www.googleapis.com/oauth2/v4/token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: serialize({
+      code: req.query.code,
+      client_id: conf.google.id,
+      client_secret: conf.google.secret,
+      redirect_uri: 'http://localhost:3000/google/callback',
+      grant_type: 'authorization_code',
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => fetch('https://www.googleapis.com/plus/v1/people/me', {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + json.access_token
+    }
+  }))
+  .then(resp => resp.json())
+  .then(json => (console.log(json), 
+    res.redirect('/welcome/'+(json.displayName || json.username))))
+  .catch(e => (console.log(e), res.status(403).end('Not Authorized')))
+}
+)
 
 app.get('/welcome/:name', (req, res) => 
   res.sendFile(path.resolve(__dirname+'/welcome.html')))
@@ -42,3 +71,16 @@ app.get('/', (req, res) =>
   res.sendFile(path.resolve(__dirname+'/index.html')))
 
 app.listen(3000)
+//body: 'code=' + req.query.code +
+  //'&client_id=' + conf.google.id +
+  //'&client_secret=' + conf.google.secret + 
+  //'&redirect_uri=http://localhost:3000/google/callback'+
+  //'&grant_type=authorization_code',
+function serialize(obj) {
+  var str = [];
+  for(var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
